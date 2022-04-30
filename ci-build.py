0bad5cc69f68a924895d1ad79862a969cdd3cff9
Jerrylum <serverofjerry@gmail.com>
2022-05-01 01:35:58 +0800

    :bug: Fix CI script on Linux

--- ci-build.py
+++ ci-build.py
@@ -1,9 +1,11 @@
 import os
+import platform
 import json
 
 f = open('profiles.json')
 data = json.load(f)
 
+prefix = "" if platform.system() == "Windows" else "./"
+
 for p in data['profiles']:
-    os.system('gradlew build -Dprofile="%s"' % (p["minecraft-version"]))
-    # os.system('gradlew build ' + ' '.join(['-D%s="%s"' % (s, p[s]) for s in p]))
+    os.system('%sgradlew build -Dprofile="%s"' % (prefix, p["minecraft-version"]))
